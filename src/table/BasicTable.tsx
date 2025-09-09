409a375446de701f03d566b2dc91593756ff0cb9
VictorSarov <victor.sarov@rentalmatics.com>
2025-09-09 12:54:01 +0100

    all good

--- src/table/BasicTable.tsx
+++ src/table/BasicTable.tsx
@@ -12,8 +12,13 @@ import {
   TableCell,
   TableHead,
   TableRow,
+  Box
 } from "@mui/material";
 import allCompanies from '../data/companies/allCompanies';
+import {
+  scoreFitchAndSpRating,
+  scoreMoodyRating,
+} from '../helpers/allOther';
 
 export type Company = {
   dateOfAnalysis: string,
@@ -49,7 +54,7 @@ const defaultColumns: ColumnDef<Company>[] = [
   },
   {
     accessorKey: "score",
-    header: () => <><div style={{fontSize: '12px'}}>{"Dmitri Score"}</div><div style={{fontSize: '10px'}}>{'(max 11)'}</div></>,
+    header: () => <><div style={{ fontSize: '12px' }}>{"Dmitri Score"}</div><div style={{ fontSize: '10px' }}>{'(max 11)'}</div></>,
     id: 'score',
     cell: info => info.getValue<number>().toFixed(2)
   },
@@ -60,7 +65,19 @@ const BasicTable = () => {
   const [sorting, setSorting] = React.useState([
     { id: "score", desc: true },
   ]);
+  const [fitchAndSp, setFitchAndSp] = React.useState('');
+  const [convertedFitchAndSp, setConvertedFitchAndSp] = React.useState(0);
 
+  const [moody, setMoody] = React.useState('');
+  const [convertedMoody, setConvertedMoody] = React.useState(0);
+
+  function convertMoody() {
+    setConvertedMoody(scoreMoodyRating(moody));
+  }
+
+  function convertFitchAndSp() {
+    setConvertedFitchAndSp(scoreFitchAndSpRating(fitchAndSp));
+  }
 
   const table = useReactTable({
     data: allCompanies,
@@ -74,34 +91,53 @@ const BasicTable = () => {
   })
 
   return (
-    <Table sx={{ maxWidth: 750 }}>
-      <TableHead>
-        {table.getHeaderGroups().map(headerGroup => (
-          <TableRow key={headerGroup.id} sx={{ backgroundColor: '#f0f0f0' }}>
-            {headerGroup.headers.map(header => (
-              <TableCell
-                key={header.id}
-              >
-                {header.isPlaceholder
-                  ? null
-                  : flexRender(header.column.columnDef.header, header.getContext())}
-              </TableCell>
-            ))}
-          </TableRow>
-        ))}
-      </TableHead>
-      <TableBody>
-        {table.getRowModel().rows.map(row => (
-          <TableRow key={row.id} sx={{ '&:hover': { backgroundColor: '#f5f5f5' } }}>
-            {row.getVisibleCells().map(cell => (
-              <TableCell key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </TableCell>
-            ))}
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <>
+      <Box sx={{ display: 'flex', gap: 10 }}>
+        <Box>
+          <input type='text' value={fitchAndSp} onChange={(e) => setFitchAndSp(e.target.value)} />
+          <button onClick={convertFitchAndSp}>convert</button>
+          <h3>Fitch and S&P: {convertedFitchAndSp}</h3>
+        </Box>
+        <Box>
+          <input
+            type="text"
+            value={moody}
+            onChange={(e) => setMoody(e.target.value)}
+          />
+          <button onClick={convertMoody}>convert</button>
+          <h3>Moody: {convertedMoody}</h3>
+        </Box>
+      </Box >
+      <hr />
+      <Table sx={{ maxWidth: 750 }}>
+        <TableHead>
+          {table.getHeaderGroups().map(headerGroup => (
+            <TableRow key={headerGroup.id} sx={{ backgroundColor: '#f0f0f0' }}>
+              {headerGroup.headers.map(header => (
+                <TableCell
+                  key={header.id}
+                >
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(header.column.columnDef.header, header.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableHead>
+        <TableBody>
+          {table.getRowModel().rows.map(row => (
+            <TableRow key={row.id} sx={{ '&:hover': { backgroundColor: '#f5f5f5' } }}>
+              {row.getVisibleCells().map(cell => (
+                <TableCell key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
   )
 }
 
