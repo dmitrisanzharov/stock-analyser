a5926fe26c78ac9c4564fde2bddbf7a9bdc95cb5
VictorSarov <victor.sarov@rentalmatics.com>
2025-09-09 11:15:18 +0100

    push

--- src/data/companies/allCompanies.ts
+++ src/data/companies/allCompanies.ts
@@ -1,6 +1,7 @@
 import oldCompanies from './oldAnalysis';
 import lennarCorp_Buffet from './lennarCorp_Buffet';
+import tsmc from './tsmc';
 
-const allCompanies = [...oldCompanies, lennarCorp_Buffet];
+const allCompanies = [...oldCompanies, lennarCorp_Buffet, tsmc];
 
 export default allCompanies;
