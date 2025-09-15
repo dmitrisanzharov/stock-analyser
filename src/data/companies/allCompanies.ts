091623bed405d6660b12018086e90765f2202caf
VictorSarov <victor.sarov@rentalmatics.com>
2025-09-15 17:30:24 +0100

    all good

--- src/data/companies/allCompanies.ts
+++ src/data/companies/allCompanies.ts
@@ -1,7 +1,8 @@
 import oldCompanies from './oldAnalysis';
 import lennarCorp_Buffet from './lennarCorp_Buffet';
 import tsmc from './tsmc';
+import bradesparSA from './bradesparSA';
 
-const allCompanies = [...oldCompanies, lennarCorp_Buffet, tsmc];
+const allCompanies = [...oldCompanies, lennarCorp_Buffet, tsmc, bradesparSA];
 
 export default allCompanies;
